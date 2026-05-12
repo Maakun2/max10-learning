a635e7117811d3b6185110f25a7bfbcf260ffdb5
Maakun2 <masayuki0608ishikawa@gmail.com>
2026-05-12 18:43:18 +0900

    Add FPGA module with LED control logic
    This module implements a simple FPGA design with counters for generating LED signals based on clock cycles.

---
+++ projects/PROJ_PARALLEL_LED/FPGA2.v
@@ -0,0 +1,73 @@
+`define CYCLE_SEC1 48000000
+`define CYCLE_SEC2 24000000
+
+//--------------------
+// Top of the FPGA
+//--------------------
+module FPGA
+(
+	input wire clk,			// 48Mhz Clock
+	input wire res_n,			//Reset Switch
+	output wire [2:0]led	//LED Output
+);
+
+//-----------------------------
+// Counter to makeS 1sec Period
+//-----------------------------
+reg [31:0] counter_1sec;
+wire       period_1sec;
+reg [31:0] counter_sec2;
+wire       period_sec2;
+//
+always @(posedge clk, negedge res_n)
+begin
+	if (~res_n)
+	begin
+		counter_1sec <= 32'h00000000;
+		counter_sec2 <= 32'h00000000;
+	end
+	else
+	begin
+		if (period_1sec)
+			counter_1sec <= 32'h00000000;
+		else
+			counter_1sec <= counter_1sec + 32'h00000001;
+			
+		if(period_sec2)
+			counter_sec2 <= 32'h00000000;
+		else
+			counter_sec2 <= counter_sec2 + 32'h00000001;
+	end	
+end
+//
+assign period_1sec =(counter_1sec == (`CYCLE_SEC1 - 1));
+assign period_sec2 =(counter_sec2 == (`CYCLE_SEC2 - 1));
+
+//----------------------------
+// Counter to make LED signal
+//----------------------------
+reg counter_led1;
+reg counter_led2;
+//
+always @(posedge clk, negedge res_n)
+begin
+	if (~res_n)
+	begin
+		counter_led1 <= 1'b0;
+		counter_led2 <= 1'b0;
+	end
+	else
+	begin
+		if (period_1sec)
+			counter_led1 <= counter_led1 + 1'b1;
+			
+		if(period_sec2)
+			counter_led2 <= counter_led2 + 1'b1;
+		end
+end
+//
+assign led[0] = ~counter_led1; // LED on by low level
+assign led[1] = ~counter_led2; 
+assign led[2] = 1'b1;
+
+endmodule
